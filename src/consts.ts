dc446585866da7e6c0c932bcc95370b70ff8fe4d
Ripdiegozz <diegogarciag63@gmail.com>
2024-04-12 00:28:53 -0500

    Cambios mayores, redisenho

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Fundaleon';
+export const SITE_DESCRIPTION = 'Bienvenido al blog de la Fundación Literaria León Bienvenido Weffer, en donde encontrarás información sobre nuestros eventos, talleres, publicaciones y mucho más.';
